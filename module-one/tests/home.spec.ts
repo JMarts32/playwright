db1afa683cac246a00d3611ac3d391a1481a3a1d
JMarts32 <j.santosa@uniandes.edu.co>
2023-07-25 18:57:30 -0500

    text and css selector tests

--- module-one/tests/home.spec.ts
+++ module-one/tests/home.spec.ts
@@ -13,8 +13,31 @@ test.describe('Home', () => {
         // Open the url
         await page.goto('https://practice.automationbro.com/about/');
 
-        //verify the title
+        // verify the title
         await expect(page).toHaveTitle('About – Practice E-Commerce Site');
     })
+
+    test('Click get started button', async ({ page }) => {
+      // Open the url 
+      await page.goto('https://practice.automationbro.com');
+
+      // Locate the button and click it
+      page.locator('#get-started').click();
+
+      // verify the url
+      await expect(page).toHaveURL('https://practice.automationbro.com/#get-started');
+    })
+
+    test('Verify heading text is visible using text selector', async ({ page }) => {
+      // Open the url
+      await page.goto('https://practice.automationbro.com');
+
+      // find the text using locator
+      const heading_test = await page.locator('text=Think different. Make different.');
+
+      //verify that the text is visible
+      await expect(heading_test).toBeVisible();
+    })
+    
     
 })
