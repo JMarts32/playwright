dbdd2ae7d31cd556107e5a6a54ab990a0f99ecc4
JMarts32 <j.santosa@uniandes.edu.co>
2023-07-29 16:56:49 -0500

    fails the updloading test becasue it takes too much time to show the message in chrome an safari

---
+++ module-one/tests/upload.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from '@playwright/test'
+const path = require('path');
+
+test.describe('', () => {
+    test('should upload a test file', async ({ page }) => {
+        // Open the url
+        await page.goto('https://practice.automationbro.com/cart');
+
+        // provide the test file path
+        const filepath =  path.join(__dirname, '../data/Luna1.jpg'); 
+
+        // upload the test file
+        await page.setInputFiles('input#upfile_1', filepath);
+
+        // click the submit button
+        page.locator('#upload_1').click();
+
+        // assertion
+        await expect(page.locator('#wfu_messageblock_header_1_label_1')).toContainText('uploaded successfully')
+    })
+    
+})
